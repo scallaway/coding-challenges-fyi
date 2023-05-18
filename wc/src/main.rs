134d2332a5adcda3acc581a61c61b399ff2df063
Scott Callaway <scott.callaway@uipath.com>
2023-05-18 10:22:01 +0100

    Add a very basic `wc -c {file_name}` implementation

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -1,3 +1,30 @@
+use std::{env, time::Instant, todo};
+
 fn main() {
-    println!("Hello, world!");
+    let now = Instant::now();
+    let args = get_args();
+
+    println!("{}", parse_args(args));
+    println!("Took: {}ns", now.elapsed().as_nanos());
+}
+
+fn get_args() -> Vec<String> {
+    return env::args().collect::<Vec<String>>()[1..].to_vec();
+}
+
+fn parse_args(args: Vec<String>) -> i64 {
+    if args.len() == 1 {
+        todo!("Must supply a flag");
+    }
+
+    if args[0] == "-c" {
+        return get_byte_count(args.get(1).unwrap());
+    }
+
+    return -1;
+}
+
+fn get_byte_count(file_name: &str) -> i64 {
+    let file = std::fs::read_to_string(file_name).expect("Unable to read file");
+    return file.len() as i64;
 }
