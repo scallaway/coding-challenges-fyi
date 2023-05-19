3481f3359c5575966cae67a2a4a16ee657d8040a
Scott Callaway <scott.callaway@uipath.com>
2023-05-19 16:36:29 +0100

    A few more tidy-up things

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -5,23 +5,31 @@ fn main() {
     let args = get_args();
 
     println!("{}", parse_args(args));
-    println!("Took: {}ns", now.elapsed().as_nanos());
+
+    // Only print the time taken in debug mode
+    if cfg!(debug_assertions) {
+        println!("Took: {}ns", now.elapsed().as_nanos());
+    }
 }
 
 fn get_args() -> Vec<String> {
     return env::args().collect::<Vec<String>>()[1..].to_vec();
 }
 
-fn parse_args(args: Vec<String>) -> i64 {
+fn parse_args(args: Vec<String>) -> String {
     if args.len() == 1 {
         todo!("Must supply a flag");
     }
 
     if args[0] == "-c" {
-        return get_byte_count(args.get(1).unwrap());
+        return format!(
+            "{} {}",
+            get_byte_count(args.get(1).unwrap()),
+            args.get(1).unwrap()
+        );
     }
 
-    return -1;
+    return "".to_string();
 }
 
 fn get_byte_count(file_name: &str) -> i64 {
